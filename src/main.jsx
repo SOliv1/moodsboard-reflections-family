7fa34035124edd1b9abe48573a1cf26e68958255
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2026-06-05 23:19:14 +0100

    Use hash routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-const routerBase = import.meta.env.PROD ? '/moodsboard-reflections-family' : undefined;
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={routerBase}>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
